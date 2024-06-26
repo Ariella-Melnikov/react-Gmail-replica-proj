7023278721574f1a4510a5c0135a351703f447f6
Elad Berliner <eladberliner@gmail.com>
2024-06-26 16:59:13 +0300

    somewhat working

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,3 +1,28 @@
-export function NoteList() {
-    return <div>note list</div>
+
+import { NotePreview } from "../cmps/NotePreview.jsx";
+
+
+
+export function NoteList({ notes ,onSelectNoteId, onRemoveNote}) {
+
+
+
+
+    return (
+<section className = "note-list-container">
+        <ul className = "note-list">
+            {notes.map(note => (
+                <li key={note.id}>
+                    <NotePreview note={note} />
+                 
+                    <section>
+                    <button onClick={() => onRemoveNote(note.id)}>Delete</button>
+                    <button >Details</button>
+                    </section>
+                </li>
+            ))}
+        </ul>
+        </section>
+
+    )
 }
--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,4 +1,55 @@
 
+// import { noteService } from "../services/note.service.js"
+// import { NoteList } from "../cmps/NoteList.jsx";
+// import { NoteFilter } from "../cmps/NoteFilter.jsx";
+// import { AddNote } from "../cmps/AddNote.jsx";
+
+
+// const { Link, useSearchParams } = ReactRouterDOM
+// const { useState, useEffect, useRef } = React
+
+
+
+
+
+// export function NoteIndex() {
+
+//     const [notes, setNotes] = useState([])
+
+
+//     useEffect(() => {
+//         noteService.query().then(notes => { setNotes(notes) })
+//     }, [])
+
+
+//     function onSelectNoteId(noteId) {
+//         setSelectedBookId(noteId)
+//     }
+//     function onRemoveNote(noteId) {
+//         noteService.remove(noteId).then(() => {
+//             setNotes((prevNotes) => prevNotes.filter((note) => note.id !== noteId))
+//         })
+//     }
+
+
+
+//     if (!notes) return <div>Loading...</div>
+
+//     return (
+//         <section className='note-index'>
+//             <React.Fragment>
+//                 <NoteFilter/>
+//                 <AddNote/>
+//                     <NoteList
+//                         notes={notes}
+//                         onRemoveNote={onRemoveNote}
+//                         onSelectNoteId={onSelectNoteId} />
+//             </React.Fragment>
+//         </section>
+
+//     )
+// }
+
 export function NoteIndex() {
     return <div>note app</div>
 }
